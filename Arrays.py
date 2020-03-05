9801774011bc4f97045e08d918a17a6d83251750
arwinlashawn <59852102+arwinlashawn@users.noreply.github.com>
2020-03-05 13:29:34 -0800

    Update Arrays.py

--- Arrays.py
+++ Arrays.py
@@ -9,7 +9,7 @@ def arrays(arr):
 # you can pass a string into strip(), then the string will be removed from the input string from the beginning and end.
 # REMEMBER, order doesn't matter for strip(), you can remove "an" and "na" if they're at the beginning and end of a
 # string.
-# split() splits a string. you can specify what to split the string by. In this case, a space. default does the same
+# split() splits a string. you can specify what to split the string by. in this case, a space. default does the same
 # thing. also, split() converts a string into list!!!
 arr = input().strip().split(' ')
 result = arrays(arr)
